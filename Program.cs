6c0cff864c68bec90d96c2b5eacd2d62f7d67203
Rahul Sarkar <rsarkar@sxiq.com.au>
2021-03-11 14:51:09 +1100

    Updating Readme and adding a default service tag json file

--- Program.cs
+++ Program.cs
@@ -9,16 +9,33 @@ namespace azure_iplookup
         static void Main(string[] args)
         {
             string ip = string.Empty;
-            if (args == null || args.Length == 0 || args.Length > 1)
+            string serviceTagJsonFileName = "ServiceTags_Public_Active.json";
+
+            if (args == null || args.Length == 0)
             {
                 Console.WriteLine("Enter IP for searching in Azure IP/ServiceTag Ranges :");
                 ip = Console.ReadLine();
+                Console.WriteLine("Enter name of the json file ( if not specified ,ServiceTags_Public_Active.json will be used ) :");
+                string userInputJsonFileName = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(userInputJsonFileName))
+                {
+                    serviceTagJsonFileName = userInputJsonFileName;
+                }
             }
             else if (args.Length == 1)
             {
                 ip = args[0];
             }
-            var azureIpAndServiceTagJson = File.ReadAllText("ServiceTags_Public_20210308.json");
+            else if (args.Length == 2)
+            {
+                ip = args[0];
+                if (!string.IsNullOrWhiteSpace(args[1]))
+                {
+                    serviceTagJsonFileName = args[1];
+                }
+            }
+
+            var azureIpAndServiceTagJson = File.ReadAllText(serviceTagJsonFileName);
             var azureIPs = JsonSerializer.Deserialize<Root>(azureIpAndServiceTagJson);
             var match = IPHelper.ReturnMatchedIPRange(ip, azureIPs.values);
             Console.WriteLine($"Matched IP Range is {match}");
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # azure-iplookup
 Lookup a given ip against Azure IP / Servicetag json file to find out if it's a valid Azure IP and find which service it belongs to
+
+# Setup and PreRequisites
+ - Requires [.net core 3.1](https://dotnet.microsoft.com/download/dotnet/3.1) runtime installed
+ - Download the latest servicet tag json from [Azure IP Ranges and Service Tags – Public Cloud](https://www.microsoft.com/en-us/download/details.aspx?id=56519)
+-  Copy it to this applications directory and rename it as ServiceTags_Public_Active.json
+
+
+# Instruction on how to use
+
+### Example 1 : 
+``` Powershell
+dotnet run 20.193.17.157
+Matched IP Range is 20.193.0.0/18 - AzureCloud.australiaeast
+```
+### Example 2 :
+
+``` Powershell
+dotnet run
+Enter IP for searching in Azure IP/ServiceTag Ranges :
+20.193.17.157
+Matched IP Range is 20.193.0.0/18 - AzureCloud.australiaeast
+```
+
+### Example 3 : Specify a **json** file
+
+``` Powershell
+dotnet run 20.193.17.157 "ServiceTags_Public_Active.json"
+Matched IP Range is 20.193.0.0/18 - AzureCloud.australiaeast
+```
